43ec3cf3988300e7bbd59a4090bd8ad373be5709
Jakub Vavra <jvavra@redhat.com>
2023-03-03 09:39:57 +0100

    Do not merge: Semi-manual test: Add test for bz2166943 and bz2143925
    When session is closed kcm forgets which ticket is active and possibly uses an old/expired one.

--- src/tests/multihost/alltests/test_kcm.py
+++ src/tests/multihost/alltests/test_kcm.py
@@ -11,6 +11,7 @@ from __future__ import print_function
 import re
 import pytest
 import time
+from pexpect import pxssh
 from sssd.testlib.common.expect import pexpect_ssh
 from sssd.testlib.common.utils import sssdTools
 from constants import ds_instance_name
@@ -119,3 +120,79 @@ class TestKcm(object):
               "and.sssd.conf...some_path.kcm.socket..don't.match"
         find = re.compile(r'%s' % msg)
         assert find.search(log)
+
+
+    def test_kcm_switches_away(self, multihost, backupsssdconf):
+        """
+        :title: IDM-SSSD-TC: kcm_provider: Kcm switches away from newest ticket
+        :id: 245ddaba-f99e-49ce-8e05-bef4008bdd12
+        :setup:
+         1. Create two user users.
+         2. Clean up all tickets
+         3. Install server with gui, enable graphical target, reboot
+        :steps:
+          1. Log into a machine and create a ticket for first user
+          2. Log into a machine and create a ticket for second user
+          3. Log into a machine and show tickets
+          4. Login in gdm as foo1@test.example
+          5. Run kswitch -p foo1 in a new terminal
+          6. Lock and unlock workstation
+          7. Run klist in the same terminal
+        :expectedresults:
+          1. Ticket for first user is created.
+          2. Ticket second user is created.
+          3. Ticket for second user is active.
+          4. Logged in as foo1
+          5. Credential is switched
+          6. Gui is locked and unlocked
+          7. Klist shows the credential foo1.
+        :customerscenario: True
+        :bugzilla:
+           https://bugzilla.redhat.com/show_bug.cgi?id=2166943
+           https://bugzilla.redhat.com/show_bug.cgi?id=2143925
+        """
+        multihost.client[0].run_command(
+            'yum install -y oddjob-mkhomedir rsyslog krb5-workstation', raiseonerr=False)
+        multihost.client[0].run_command(
+            'systemctl enable oddjobd; systemctl start oddjobd;', raiseonerr=False)
+        multihost.client[0].run_command(
+            'yum groupinstall "Server with GUI" -y || yum groupinstall -y gnome-desktop',
+            raiseonerr=False
+        )
+        multihost.client[0].run_command(
+            'systemctl set-default graphical', raiseonerr=False)
+        multihost.client[0].run_command(
+            'authconfig --enablemkhomedir --update || authselect select sssd with-mkhomedir --force',
+            raiseonerr=False
+        )
+        multihost.client[0].run_command('reboot', raiseonerr=False)
+        time.sleep(120)
+        client = sssdTools(multihost.client[0])
+        client.sssd_conf('kcm', {'debug_level': '9'})
+        client.sssd_conf('sssd', {'services': 'nss, pam, ssh'})
+        user1 = f"foo1@example.test"
+        dom_section = f'domain/{client.get_domain_section_name()}'
+        sssd_params = {
+            'access_provider': 'simple',
+            'simple_allow_users': 'foo1',
+            'auth_provider': 'krb5',
+            'id_provider': 'ldap',
+            'chpass_provider': 'krb5',
+            'krb5_realm': 'EXAMPLE.TEST',
+            'krb5_server': multihost.master[0].sys_hostname,
+            'fallback_homedir': '/home/%u',
+        }
+        client.sssd_conf(dom_section, sssd_params)
+        client.clear_sssd_cache()
+        multihost.client[0].run_command("systemctl restart gdm || systemctl restart lightdm", raiseonerr=False)
+        multihost.client[0].run_command(f"cat /etc/sssd/sssd.conf", raiseonerr=False)
+        multihost.client[0].run_command(f"su - {user1} -c 'kdestroy -A'", raiseonerr=False)
+        multihost.client[0].run_command(
+             f"su - {user1} -c 'kinit -c KCM:14583101:12345 foo1'", stdin_text="Secret123", raiseonerr=False)
+        multihost.client[0].run_command(f"su - {user1} -c 'klist -l'", raiseonerr=False)
+        multihost.client[0].run_command(
+             f"su - {user1} -c 'kinit -c KCM:14583101:23456 foo2'", stdin_text="Secret123", raiseonerr=False)
+        multihost.client[0].run_command(f"su - {user1} -c 'klist -l'", raiseonerr=False)
+        multihost.client[0].run_command(f"su - {user1} -c 'kswitch -p foo1'", raiseonerr=False)
+        multihost.client[0].run_command(f"su - {user1} -c 'klist'", raiseonerr=False)
+        assert False, """Use --pdb to stop here, proceed with manual testing."""
